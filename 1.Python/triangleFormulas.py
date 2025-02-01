93e5be22e29edc29dab9ba1eec1b59626bc268e3
Karthikeyan R <karthikeyan.rk@gmail.com>
2025-02-02 01:21:32 +0800

    Add files via upload
    OOPS - Class related assignment files are uploaded

---
+++ 1.Python/triangleFormulas.py
@@ -0,0 +1,9 @@
+class CalculateTriangleFormulas():
+    
+    def calculateArea(height,breadth): #Procedure 1
+        area=(height*breadth)/2
+        return area
+
+    def calculatePerimeter(height1,height2,breadth): #Procedure 2
+        perimeter=height1+height2+breadth
+        return perimeter
